bb11a7cf6938836fbbb11df481a59a6fb482244b
Eric Cool <coolazura@gmail.com>
2019-08-07 17:00:49 -0400

    initial git

---
+++ PipedOutputPollutedElectrolyzerPatchPatches.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using Harmony;
+using UnityEngine;
+using Nightinggale.PipedOutput;
+using PollutedElectrolyzer;
+
+namespace PipedOutputPollutedElectrolyzerPatchPatches {
+    public class PipedOutputPollutedElectrolyzerPatchPatches {
+
+        public static void AddPollutedElectrolyzer(GameObject go)
+		{
+            var AddOutput = Traverse.Create(typeof(ApplyExhaust)).Method("AddOutput", new[] { typeof(GameObject), typeof(CellOffset), typeof(SimHashes) });
+			AddOutput.GetValue(go, new CellOffset(1, 1), SimHashes.ContaminatedOxygen);
+            AddOutput.GetValue(go, new CellOffset(0, 1), SimHashes.Hydrogen);
+		}
+
+        [HarmonyPatch(typeof(PollutedElectrolyserConfig), "DoPostConfigurePreview")]
+		public static class PollutedElectrolyserConfig_DoPostConfigurePreview_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				AddPollutedElectrolyzer(go);
+			}
+		}
+
+		// Token: 0x02000029 RID: 41
+		[HarmonyPatch(typeof(PollutedElectrolyserConfig), "DoPostConfigureUnderConstruction")]
+		public static class PollutedElectrolyserConfig_DoPostConfigureUnderConstruction_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				AddPollutedElectrolyzer(go);
+			}
+		}
+
+		// Token: 0x0200002A RID: 42
+		[HarmonyPatch(typeof(PollutedElectrolyserConfig), "ConfigureBuildingTemplate")]
+		public static class PollutedElectrolyserConfig_ConfigureBuildingTemplate_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				AddPollutedElectrolyzer(go);
+			}
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# PipedOutputPollutedElectrolyzerPatch
+
+A patch to add `PipedOutput` to `PollutedElectrolyzer`.
+
+# How To Use
+* This mod needs to be loaded after `PipedOutput` and `PollutedElectrolyzer` in the Mods Page.
+
+# References
+* [PipedOutput](https://github.com/Nightinggale/ONI-mods/tree/master/src/PipedOutput) by [@Nightinggale](https://github.com/Nightinggale)
+* [PollutedElectrolyzer](https://github.com/Pholith/ONI-Mods/tree/master/src/PollutedElectrolyzer) by [@Pholith](https://github.com/Pholith)
