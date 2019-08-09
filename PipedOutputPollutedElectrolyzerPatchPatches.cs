6402a957262761da024d23a01cab1c96eb1d1056
Eric Cool <coolazura@gmail.com>
2019-08-09 13:59:04 -0400

    fix indentation

--- PipedOutputPollutedElectrolyzerPatchPatches.cs
+++ PipedOutputPollutedElectrolyzerPatchPatches.cs
@@ -13,39 +13,39 @@ namespace PipedOutputPollutedElectrolyzerPatchPatches {
     public class PipedOutputPollutedElectrolyzerPatchPatches {
 
         public static void AddPollutedElectrolyzer(GameObject go)
-		{
+        {
             var AddOutput = Traverse.Create(typeof(ApplyExhaust)).Method("AddOutput", new[] { typeof(GameObject), typeof(CellOffset), typeof(SimHashes) });
-			AddOutput.GetValue(go, new CellOffset(1, 0), SimHashes.ContaminatedOxygen);
+            AddOutput.GetValue(go, new CellOffset(1, 0), SimHashes.ContaminatedOxygen);
             AddOutput.GetValue(go, new CellOffset(1, 1), SimHashes.Hydrogen);
-		}
+        }
 
         [HarmonyPatch(typeof(PollutedElectrolyserConfig), "DoPostConfigurePreview")]
-		public static class PollutedElectrolyserConfig_DoPostConfigurePreview_Patch
-		{
-			public static void Postfix(GameObject go)
-			{
-				AddPollutedElectrolyzer(go);
-			}
-		}
+        public static class PollutedElectrolyserConfig_DoPostConfigurePreview_Patch
+        {
+            public static void Postfix(GameObject go)
+            {
+                AddPollutedElectrolyzer(go);
+            }
+        }
 
-		// Token: 0x02000029 RID: 41
-		[HarmonyPatch(typeof(PollutedElectrolyserConfig), "DoPostConfigureUnderConstruction")]
-		public static class PollutedElectrolyserConfig_DoPostConfigureUnderConstruction_Patch
-		{
-			public static void Postfix(GameObject go)
-			{
-				AddPollutedElectrolyzer(go);
-			}
-		}
+        // Token: 0x02000029 RID: 41
+        [HarmonyPatch(typeof(PollutedElectrolyserConfig), "DoPostConfigureUnderConstruction")]
+        public static class PollutedElectrolyserConfig_DoPostConfigureUnderConstruction_Patch
+        {
+            public static void Postfix(GameObject go)
+            {
+                AddPollutedElectrolyzer(go);
+            }
+        }
 
-		// Token: 0x0200002A RID: 42
-		[HarmonyPatch(typeof(PollutedElectrolyserConfig), "ConfigureBuildingTemplate")]
-		public static class PollutedElectrolyserConfig_ConfigureBuildingTemplate_Patch
-		{
-			public static void Postfix(GameObject go)
-			{
-				AddPollutedElectrolyzer(go);
-			}
-		}
+        // Token: 0x0200002A RID: 42
+        [HarmonyPatch(typeof(PollutedElectrolyserConfig), "ConfigureBuildingTemplate")]
+        public static class PollutedElectrolyserConfig_ConfigureBuildingTemplate_Patch
+        {
+            public static void Postfix(GameObject go)
+            {
+                AddPollutedElectrolyzer(go);
+            }
+        }
     }
 }
